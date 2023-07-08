cf2b2d6b37416fef7284fb7a2b11a6671cca0f86
Sydney Pogue <spogue1441@gmail.com>
2023-07-08 11:31:45 -1000

    added contact page

---
+++ demo-fork-testing-app/src/app/contact/page.js
@@ -0,0 +1,15 @@
+"use client";
+
+export default function ContactFeature() {
+
+  return (
+    <main>
+      <ul>Name: Acme Corporation</ul>
+      <ul>Phone Number: 555-987-6543</ul>
+      <ul>Street Address: 456 Elm Street</ul>
+      <ul>State: New York</ul>
+      <ul>City: New York City</ul>
+      <ul>Zipcode: 10001</ul>
+    </main>
+  )
+}
