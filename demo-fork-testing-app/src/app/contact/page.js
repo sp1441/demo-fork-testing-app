37fbd435f20d1d323f9c82838b4e623dc2b48ffb
Sydney Pogue <spogue1441@gmail.com>
2023-07-08 11:49:54 -1000

    added contact page

---
+++ demo-fork-testing-app/src/app/contact/page.js
@@ -0,0 +1,12 @@
+export default function ContactFeature() {
+  return (
+    <main>
+      <ul>Name: Toys R Us</ul>
+      <ul>Phone Number: (808)567-7777</ul>
+      <ul>Street Address: 123 Toy St. </ul>
+      <ul>State: Florida</ul>
+      <ul>City: Miami</ul>
+      <ul>Zipcode: 02334</ul>
+    </main>
+  )
+}
